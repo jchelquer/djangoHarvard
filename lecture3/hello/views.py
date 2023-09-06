e8792176bcc1c7211736aac65cbf0ff56ff017e1
José Chelquer <jose@chelquer.com.ar>
2023-09-05 22:15:23 -0300

    Mensaje de algo

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -4,8 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello vía HttpResponse puesto en la función index en views.py de la app hello")
+
 def saludar(request, nombre):
-    return HttpResponse(f"<H2>Hola {nombre}</H2>")
+   #  return HttpResponse(f"<H2>Hola {nombre}</H2>")
+    nombre=nombre.capitalize()
+    return render(request, "hello/saludar.html", {
+       "nombreEnPlantilla": nombre
+    } )
 def saludarNombreEdad(request, nombre, edad):
     return HttpResponse(f"Hola {nombre}, veo que tienes {edad} años.")
 
